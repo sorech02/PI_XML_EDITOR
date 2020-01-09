bd389b823a8954e19e774a8563e81ad778b9956f
KRAFFT Baptiste <baptiste.krafft@telecomnancy.eu>
2020-01-09 16:13:44 +0100

    Add File Page No Layout

--- my-app/src/app/add-file/add-file.component.ts
+++ my-app/src/app/add-file/add-file.component.ts
@@ -14,51 +14,66 @@ export class AddFileComponent implements OnInit {
   xmlCollection;
   jsonDoc;
   xmlParser;
-  codeset: Codeset;
   document$;
-  json; 
+  json;
   constructor(db: AngularFirestore) { 
     this.xmlCollection = db.collection("XmlFile");
+    this.xmlParser = new xmlParser();
+  }
+ 
+  ngOnInit() {
+  }
+
+
+
+  downloadFile(){
     this.json = this.xmlCollection.doc("Vaccination CVX Code").valueChanges();
     this.json.subscribe(value => {
       var docJson = JSON.stringify(value);// create a json string from object codeset
       docJson = JSON.parse(docJson); // create json objectz
-      console.log(docJson);
       var xml = "<codeset>\n";
       xml +=  OBJtoXML(docJson, "  "); //JsonToXML.parse("codeset", docJson); // create xml object from json 
       xml += "\n</codeset>";  
       var file = new File([xml  ], value.label + ".xml", {type: "text/plain;charset=utf-8"} );
       saveAs(file);
     });
-    
-    /*
-    
-    console.log("docJson", docJson);*/
-  }
- 
-  ngOnInit() {
-  }
-
-  ngOnDestroy() {
     this.json.unsubscribe();
+
   }
 
   // Add document from url
   onSubmitForm(form: NgForm){
-    this.xmlParser = new xmlParser(form.value['url']); // Create the xml parser
-    this.codeset = this.xmlParser.getCodeset(); // Put the data into the object
-    this.jsonDoc = JSON.stringify(this.codeset); // Create a json string from the codeset
+     // Create the xml parser
+    this.convertXmlIntoJsonAndSendItToCollection( this.xmlParser.getCodesetWithUrl(form.value['url']) ); // Put the data into the object
+
+  }
+
+  //Add document from File
+  readFile = (e) => {
+    console.log('incoming file');
+    const file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+        const xmlData: string = (evt as any).target.result;
+        console.log("xml data" + xmlData);
+        this.convertXmlIntoJsonAndSendItToCollection(this.xmlParser.getCodesetWithFile(xmlData));
+    };
+    reader.readAsText(file);
+}
+
+  convertXmlIntoJsonAndSendItToCollection(codeset){
+    this.jsonDoc = JSON.stringify(codeset); // Create a json string from the codeset
     this.jsonDoc = JSON.parse(this.jsonDoc); // Convert Json String into Json Object
-    console.log(this.jsonDoc);
     // Then add the json object into data base: 
-    this.addDocumentIntoCollection(this.xmlCollection, this.codeset.label, this.jsonDoc);
+    this.addDocumentIntoCollection(this.xmlCollection, codeset.label, this.jsonDoc);
   }
 
   getDocument(){
     this.document$ = this.xmlCollection.valueChanges();
-    
   }
-
 
   // Add o document to a collection
   addDocumentIntoCollection(collection, documentName:string, data){
@@ -68,53 +83,58 @@ export class AddFileComponent implements OnInit {
 
 function OBJtoXML(obj, ident) {
   var xml = '';
+  var re = /\_/gi;
   for (var prop in obj) {
       
       if(obj[prop] instanceof Array) {
         if (prop == "references") {
-          xml += ident + "<" + prop + ">" + "\n";
+          xml += ident + "<" + prop.replace(re, '-') + ">" + "\n";
           for (var array in obj[prop]){
             var object = new Object(obj[prop][array]);
             xml += ident + "  "+ "<link-to codeset=" + object["type"] + ">" + object["codeValue"] + "</link-to>\n"; 
           }
-          xml += ident + "</" + prop + ">" + "\n";
+          xml += ident + "</" + prop.replace(re, '-') + ">" + "\n";
         }
         else {  
           for (var array in obj[prop]) {
-                xml += ident + "<" + prop + ">" + "\n"; 
+                xml += ident + "<" + prop.replace(re, '-') + ">" + "\n"; 
                 xml += OBJtoXML(new Object(obj[prop][array]), ident + "  ");
-                xml += ident + "</" + prop + ">\n";
+                xml += ident + "</" + prop.replace(re, '-') + ">\n";
             }
         }
         
       } else if (typeof obj[prop] == "object") {
           if (prop == "use_age" || prop == "use_date"){
             var first_time = true;
-            for (var element in obj[prop]){
-                if (obj[element] != {} && obj[element] != undefined){
-                  if (first_time == true) {
-                    xml += ident + "<" + prop + ">" + "\n";
-                    first_time = false;
-                  }
-                  xml += ident + "  " + "<" + element + ">";
-                  xml += obj[element];
-                  xml += "</" + element + ">\n"; 
+            
+            
+              var object = new Object(obj[prop]);
+              for (var o in object){
+                
+                if (first_time == true) {
+                  xml += ident + "<" + prop.replace(re, '-') + ">" + "\n";
+                  first_time = false;
                 }
+                xml += ident + "  " + "<" + o.replace(re, '-') + ">";
+                xml += object[o];
+                xml += "</" + o.replace(re, '-') + ">\n"; 
               }
+              
+            
             if (first_time == false) {
-              xml += ident + "</" + prop + ">" + "\n";
+              xml += ident + "</" + prop.replace('_', '-') + ">" + "\n";
             }
           }
           else {
-            xml += ident + "<" + prop + ">" + "\n";
+            xml += ident + "<" + prop.replace(re, '-') + ">" + "\n";
             xml += OBJtoXML(new Object(obj[prop]), ident + "  ");
-            xml += ident +  "</" + prop + ">\n";
+            xml += ident +  "</" + prop.replace(re, '-') + ">\n";
           }
       } else {
         if (obj[prop] != '' && obj[prop] != null) {
-          xml += ident + "<" + prop + ">";
+          xml += ident + "<" + prop.replace(re, '-') + ">";
           xml += obj[prop];
-          xml += "</" + prop + ">\n";
+          xml += "</" + prop.replace(re, '-') + ">\n";
         }
       }
       
